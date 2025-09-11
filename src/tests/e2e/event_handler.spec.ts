f2290c95cdbcd038761e16404a818c50090d690c
faizul-bs23 <faizulcse@gmail.com>
2025-09-11 21:22:20 +0600

    parameterise and event handling

---
+++ src/tests/e2e/event_handler.spec.ts
@@ -0,0 +1,16 @@
+import {test} from "@playwright/test";
+
+test.describe("Event Handler Tests", () => {
+
+    test("Download event test", async ({page}) => {
+        await page.goto("https://www.messenger.com/desktop");
+
+        const downloadEvent  = page.waitForEvent("download");
+        await page.getByRole('button', {name: 'Download'}).click();
+        const downloadPage = await downloadEvent;
+        console.log(`Download URL: ${downloadPage.url()}`);
+        await downloadPage.saveAs(`./downloads/${downloadPage.suggestedFilename()}`);
+
+
+    })
+});
